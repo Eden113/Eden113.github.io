5eaebc11e9fc2b8fb6403628828710eded154db0
Eden113 Berthelot <edenberthelot6@gmail.com>
2023-09-05 13:04:01 +0000

    Platformer

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -55,16 +55,16 @@ $(function () {
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
-    createCollectable("database", 500, 300, 20, 0.5);
-    createCollectable("grace", 350, 200, 300);
-    createCollectable("max", 400, 600, 20, 1);
+    createCollectable("database", 1200, 300, 20, 0.2);
+    createCollectable("grace", 450, 110, 0.1);
+    createCollectable("max", 400, 600, 20, 0.5);
 
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon("right", 700, 1000);
     createCannon("left", 600, 2000);
     createCannon("right", 600, 2000);
     createCannon("left", 200, 1000);
