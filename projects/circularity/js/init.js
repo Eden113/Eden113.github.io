c9181a336c962fd7175ef73e93941e56ab6e60e0
Eden113 Berthelot <edenberthelot6@gmail.com>
2023-11-29 14:40:56 +0000

    circles

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -30,7 +30,9 @@ var init = function (window) {
             circles.push(circle);
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+            drawCircle(5);
+            
+            
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -43,10 +45,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+        
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+           game.checkCirclePosition(circles[0]);
+           game.checkCirclePosition(circles[1]);
+           game.checkCirclePosition(circles[2]);
+           game.checkCirclePosition(circles[3]);
+           game.checkCirclePosition(circles[4]);
 
             // TODO 9 : Iterate over the array
            
